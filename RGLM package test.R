de9ddd843c37e347e11f30cf7470b7deb38115b3
starlightxx <lightmonths@hotmail.com>
2017-05-25 23:15:38 +1000

    nothing much

--- RGLM package test.R
+++ RGLM package test.R
@@ -2,8 +2,12 @@ library(randomGLM)
 library(tidyverse)
 library(data.table)
 
-data_model <- fread('Data/fire_contents_model.csv', na.strings=c(''))
-data_test <- fread('Data/fire_contents_test.csv', na.strings=c(''))
+setwd('C:/Users/Lance/Dropbox/Programming/R working directory/')
+data_table <- fread('./Data/fire_contents.csv', na.strings=c(''), stringsAsFactors = T)
+data_frame <- as.data.frame(data_table)
+
+factor_list <- c("company", "channel", "yearsInsured", "stateRisk", "contStockOtherSI", "")
+
 
 num_format <- function(df) {
   for (i in names(df)){
@@ -12,10 +16,11 @@ num_format <- function(df) {
     }
   }
 }
-num_format(data_test)
+
+num_format(data_frame)
 
 
-
+data_frame["company"] <- as.factor(data_frame$"company")
 
 
 
